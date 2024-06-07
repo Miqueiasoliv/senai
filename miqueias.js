194658bd67052afb1820b14082258fea0ef71b50
Miqueiasoliv <miqueiasoliveira761@gmail.com>
2024-06-07 14:03:13 -0500

    save

---
+++ miqueias.js
@@ -0,0 +1,14 @@
+var numero1 = 400
+var numero2 = 500
+
+var resultado = numero1 + numero2
+
+document.write(resultado)
+
+var soma = prompt("qual o valor da soma:")
+
+document.write(soma)
+
+var media = (numero1 + numero2) / 2
+
+document.write(media)
