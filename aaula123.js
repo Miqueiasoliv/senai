ed87d87810515344b1772bc5c618f2c8aea47cb5
Miqueiasoliv <miqueiasoliveira761@gmail.com>
2024-06-20 14:31:55 -0500

    aula123

---
+++ aaula123.js
@@ -0,0 +1,6 @@
+var numero = 0
+for(numero; numero <= 700; numero=numero+1){
+    if(numero<90){
+        document.write(numero)
+    }
+}
---
+++ abc.js
@@ -0,0 +1,4 @@
+var numero = 0
+for(numero; numero <= 20; numero=numero+2){
+    document.write(numero)
+}
